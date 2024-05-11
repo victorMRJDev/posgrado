8b129cc4defc3adeed03e90cc9a5e8bc37bceb80
JoanReva <joanrevajoanreva@gmail.com>
2024-05-11 03:15:49 -0600

    Añadido CardModal

--- src/components/Cards/CardView.jsx
+++ src/components/Cards/CardView.jsx
@@ -1,21 +1,46 @@
 import React, { useState } from "react";
+import Modal from "./CardModal";
 
-const CardView = ({ image, name }) => {
+const CardView = ({ image, name, semblanza }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCardClick = () => {
+    if (semblanza) {
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <div
-      className={`bg-white rounded-lg shadow-xl p-4 transition-transform duration-300${
-        isHovered
-          ? "transform hover:scale-105 shadow-xl filter drop-shadow-lg"
-          : ""
-      }`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <img src={image} alt={name} className="w-full h-auto rounded-lg mb-2" />
-      <p className="text-lg font-semibold text-gray-800 text-center">{name}</p>
-    </div>
+    <>
+      <div
+        className={`bg-white rounded-lg shadow-xl p-4 transition-transform duration-300${
+          isHovered
+            ? " transform hover:scale-105 shadow-xl filter drop-shadow-lg"
+            : ""
+        }`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={handleCardClick}
+      >
+        <img src={image} alt={name} className="w-full h-auto rounded-lg mb-2" />
+        <p className="text-lg font-semibold text-gray-800 text-center select-none">
+          {name}
+        </p>
+      </div>
+      {isModalOpen && (
+        <Modal
+          image={image}
+          name={name}
+          semblanza={semblanza}
+          closeModal={closeModal}
+        />
+      )}
+    </>
   );
 };
 
--- src/pages/NucleoAcademico/NucleoAcademico.jsx
+++ src/pages/NucleoAcademico/NucleoAcademico.jsx
@@ -18,12 +18,18 @@ import DrMorales from "../../assets/nucleoacademico/DrMorales.jpeg";
 import LicGonzalez from "../../assets/nucleoacademico/LicGonzalez.jpeg";
 import MCValencia from "../../assets/nucleoacademico/MCValencia.jpeg";
 //
+
 const data = [
   { image: DrCampos, name: "Dr. Wilfrido Campos Francisco" },
   { image: DraCastro, name: "Dra. Mirna Castro Bello" },
   { image: DrGonzalez, name: "Dr. Mario González Durán" },
   { image: DraHernandez, name: "Dra. Mercedes Hernández De La Cruz" },
-  { image: DrMarmolejo, name: "Dr. Carlos Virgilio Marmolejo Vega" },
+  {
+    image: DrMarmolejo,
+    name: "Dr. Carlos Virgilio Marmolejo Vega",
+    semblanza:
+      "Maestro en Ciencias área Matemática Educativa por el Centro de Investigación y Estudios Avanzados del I.P.N., profesor de educación media básica, media superior y normal de 1963 a 1970, Profesor de Cálculo y Análisis Instrumental en la Facultad de Ciencias Químicas de la Universidad Autónoma de Guerrero de 1970 a 1978, Secretario General de la Universidad Autónoma de Guerrero de 1975 a 1978, Miembro fundador de la Facultad de Matemáticas de la Universidad Autónoma de Guerrero 1978, Miembro fundador del Instituto Tecnológico de Chilpancingo.\nProfesor de Cálculo Diferencial, Cálculo Integral, Cálculo vectorial, Ecuaciones Diferenciales y Álgebra lineal.\nProfesor con reconocimiento a perfil deseable, Miembro del Padrón estatal de Investigadores del Consejo de Ciencia y Tecnología del Estado de Guerrero, Miembro del Sistema de Investigadores Docentes de la Secretaría de Educación Guerrero.\nÁreas de Investigación: Ciencias Pedagógicas y Ciencias Ambientales. Autor de artículos científicos en esas áreas. \nFundador de la Maestría en Ciencias de la Ingeniería del Instituto Tecnológico de Chilpancingo.",
+  },
   { image: DrMorales, name: "Dr. Cornelio Morales Morales" },
   { image: DrFuentes, name: "Dr. Jorge Fuentes Pacheco" },
   { image: DrLuna, name: "Dr. Jesús Antonio Luna Alvarez" },

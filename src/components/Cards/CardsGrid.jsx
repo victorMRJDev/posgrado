669ae79bae70a0b794f4004ed4019f8c681142de
victorMRJDev <victortecno98@gmail.com>
2024-05-12 16:36:29 -0600

    Edit: CardModal

--- src/components/Cards/CardsGrid.jsx
+++ src/components/Cards/CardsGrid.jsx
@@ -3,7 +3,7 @@ import CardView from "./CardView";
 
 const CardsGrid = ({ data }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 cursor-pointer">
       {data.map((item, index) => (
         <CardView
           key={index}

5e4371bc9830751e7d0d623acda43fdebe2979e5
victorMRJDev <victortecno98@gmail.com>
2024-05-12 22:41:20 -0600

    Edit[LogoBar]: se agregó logo del Gobierno

--- src/components/TopBar/LogoBar.jsx
+++ src/components/TopBar/LogoBar.jsx
@@ -3,12 +3,20 @@ import logo_MCI from "../../assets/logo_MCI.jpeg";
 import logo_azul_tecnm from "../../assets/logo_azul_tecnm.png";
 import logo_itch from "../../assets/logo_itch.png";
 import logo_sep_rojo from "../../assets/logo_sep_rojo.png";
+import logo_gobierno from '../../assets/gobierno_logo.png';
 import { APP_VERSION } from "../../Global";
 
 const LogoBar = () => {
   return (
     <>
       <div className="bg-white flex justify-between items-center max-sm:hidden sm:hidden md:hidden lg:flex p-2">
+      <div className="flex items-center justify-center w-1/4 max-w-screen-lg mx-auto">
+          <img
+            className="h-20 max-w-full object-contain mx-4"
+            src={logo_gobierno}
+            alt="logo_gobierno"
+          />
+        </div>
         <div className="flex items-center justify-center w-1/4 max-w-screen-lg mx-auto">
           <img
             className="h-20 max-w-full object-contain mx-4"

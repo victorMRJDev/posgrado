681645c37c20df3cebe6e19471f805b82c78ece1
JoanReva <joanrevajoanreva@gmail.com>
2024-06-22 19:49:07 -0600

    Actualización margen StreamableVideo en Home

--- src/components/Secciones/Contenido.jsx
+++ src/components/Secciones/Contenido.jsx
@@ -3,8 +3,10 @@ import PanelLateral from "../PanelLateral/PanelLateral";
 
 const Contenido = ({ children, mostrarPanelLateral = true }) => {
   return (
-    <div className="flex flex-row m-9 gap-10">
-      <div className="w-full h-auto py-6 shadow-xl mb-4">{children}</div>
+    <div className="flex flex-row m-5 gap-10 lg:m-9">
+      <div className="w-full h-auto py-6 shadow-xl mb-4 bg-blue-500">
+        {children}
+      </div>
       {mostrarPanelLateral && <PanelLateral />}
     </div>
   );

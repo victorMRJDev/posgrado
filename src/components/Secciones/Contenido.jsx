b53d27c02203f55f0fbf4460f66578fbfaab9be0
JoanReva <joanrevajoanreva@gmail.com>
2024-06-23 05:59:42 -0600

    Mejoras en NavBar y CONTENIDO

--- src/components/Secciones/Contenido.jsx
+++ src/components/Secciones/Contenido.jsx
@@ -4,7 +4,7 @@ import PanelLateral from "../PanelLateral/PanelLateral";
 const Contenido = ({ children, mostrarPanelLateral = true }) => {
   return (
     <div className="flex flex-row m-5 gap-10 lg:m-9">
-      <div className="w-full h-auto py-6 shadow-xl mb-4">{children}</div>
+      <div className="w-full h-auto py-1 shadow-xl mb-4">{children}</div>
       {mostrarPanelLateral && <PanelLateral />}
     </div>
   );

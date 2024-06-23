7e28523676a343222f49f6f8588a0419fb05fd3c
JoanReva <joanrevajoanreva@gmail.com>
2024-06-23 04:13:23 -0600

    Modificacion CardSocial para Móvil

--- src/components/Cards/CardSocial.jsx
+++ src/components/Cards/CardSocial.jsx
@@ -4,7 +4,7 @@ const CardSocial = ({ image, texto, icon }) => {
   return (
     <>
       {/* <div className="flex flex-col w-60 h-64 lg:w-60 lg:h-64 md:w-48 md:h-52 sm:w-40 sm:h-44 max-sm:w-20 max-sm:h-28 justify-stretch shadow-2xl bg-white rounded-2xl transition-all duration-700 hover:scale-110 cursor-pointer"> */}
-      <div className="flex flex-col w-60 h-64 lg:w-60 lg:h-64 md:w-48 md:h-52 sm:w-40 sm:h-44 max-sm:w-28 max-sm:h-28 shadow-2xl items-center rounded-2xl transition-all duration-700 hover:scale-110 cursor-pointer">
+      <div className="flex flex-col w-60 h-64 lg:w-60 lg:h-64 md:w-48 md:h-52 sm:w-40 sm:h-44 max-sm:w-24 max-sm:h-24 shadow-2xl items-center rounded-2xl transition-all duration-700 hover:scale-110 cursor-pointer">
         <div className="w-full h-full bg-white rounded-2xl sm:block hidden">
           <img src={image} alt={texto} className="w-full h-full rounded-2xl" />
         </div>

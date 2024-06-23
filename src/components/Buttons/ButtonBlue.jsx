75348d9a315fc123e46cda2e624e69d6b76ae282
JoanReva <joanrevajoanreva@gmail.com>
2024-06-23 04:28:30 -0600

    Lista responsiva y ButtonBlue ajustado

--- src/components/Buttons/ButtonBlue.jsx
+++ src/components/Buttons/ButtonBlue.jsx
@@ -11,7 +11,7 @@ const ButtonBlue = ({ text, type = "button", onClick, className }) => {
     <div className={className}>
       <button
         type={type}
-        className="w-full self-center font-button text-xl bg-blue-900 py-3 rounded-lg text-white focus:outline-none focus:ring-3 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-800 transition-colors duration-300 ease-in-out"
+        className="w-full self-center font-button text-xl bg-blue-900 py-3 px-5 rounded-lg text-white focus:outline-none focus:ring-3 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-800 transition-colors duration-300 ease-in-out"
         onClick={handleClick}
       >
         {text}
--- src/components/Secciones/Lista.jsx
+++ src/components/Secciones/Lista.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Lista = ({ lista }) => {
   return (
-    <div className="flex flex-col px-10 justify-center">
+    <div className="flex flex-col px-10 justify-center max-sm:px-5 max-md:px-7">
       <ul className="text-xl text-justify font-serif font-light mt-9 text-black list-disc pl-6 space-y-3 max-sm:text-base max-md:text-lg">
         {lista.map((item, index) => (
           <li key={index}>{item}</li>

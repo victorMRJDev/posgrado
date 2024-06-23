3d98d229bc3f0d91aa5622fb3e3964e24893b83b
JoanReva <joanrevajoanreva@gmail.com>
2024-06-23 03:40:19 -0600

    CardSocial Dinámico y Multivista

--- src/components/Cards/CardSocial.jsx
+++ src/components/Cards/CardSocial.jsx
@@ -3,14 +3,19 @@ import React from "react";
 const CardSocial = ({ image, texto, icon }) => {
   return (
     <>
-      <div className="flex w-60 justify-center shadow-2xl bg-white flex-col rounded-2xl h-80 transition-all duration-700 hover:scale-110 cursor-pointer">
-        <div className="w-60 bg-white rounded-2xl">
-          <img src={image} alt="service" className="h-3/3 w-60 rounded-2xl" />
+      {/* <div className="flex flex-col w-60 h-64 lg:w-60 lg:h-64 md:w-48 md:h-52 sm:w-40 sm:h-44 max-sm:w-20 max-sm:h-28 justify-stretch shadow-2xl bg-white rounded-2xl transition-all duration-700 hover:scale-110 cursor-pointer"> */}
+      <div className="flex flex-col w-60 h-64 lg:w-60 lg:h-64 md:w-48 md:h-52 sm:w-40 sm:h-44 max-sm:w-28 max-sm:h-28 shadow-2xl items-center rounded-2xl transition-all duration-700 hover:scale-110 cursor-pointer">
+        <div className="w-full h-full bg-white rounded-2xl sm:block hidden">
+          <img src={image} alt={texto} className="w-full h-full rounded-2xl" />
         </div>
-        <div className="flex h-1/3 justify-center items-center">
-          <p className="font-sans text-black text-3xl font-subtitle">{texto}</p>
+        <div className="flex h-full justify-center items-center sm:block hidden">
+          <p className="font-sans text-black text-3xl lg:text-3xl md:text-2xl text-base">
+            {texto}
+          </p>
         </div>
-        <div className="flex justify-center mb-5">{icon}</div>
+        <div className="flex h-full justify-center items-center text-3xl lg:text-3xl md:text-2xl sm:text-xl max-sm:text-5xl mb-4 max-sm:my-4">
+          {icon}
+        </div>
       </div>
     </>
   );

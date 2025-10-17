d65ac1045851924fcc1e7c834569dd5c85e5feaf
degvalencia530-tech <degvalencia530@gmail.com>
2025-10-16 18:23:33 -0600

    add info

--- src/pages/Estudiantes/Estudiantes.jsx
+++ src/pages/Estudiantes/Estudiantes.jsx
@@ -119,29 +119,31 @@ const generaciones = [
         id: 15,
         name: "Brandon Aldahir Jacobo Salinas",
         image: fondo,
-        // semblanza: "Semblanza de Brandon...",
+        semblanza: "Diseño de un modelo espacio-temporal de la precipitación pluvial en el municipio de Chilpancingo, Guerrero. \n\nEsta investigación propone un modelo espacio-temporal de pronóstico estadístico de la precipitación pluvial con 30 años de datos históricos como mínimo en el municipio de Chilpancingo de los Bravo, Guerrero; mediante series de tiempo con análisis de Fourier y polígonos de Thiessen con Sistemas de Información Geográfica. Esto con el fin de proporcionar información relevante para la toma de desiones en la gestión del recurso hídrico, en el municipio.",
       },
       {
         id: 16,
         name: "Mario Alberto Duque Peralta",
         image: fondo,
-        // semblanza: "Semblanza de Mario...",
+        semblanza: "SISTEMA DE MONITOREO AMBIENTAL EN TIEMPO REAL PARA CULTIVOS DE AGUACATE A CIELO ABIERTO. \n\nEsta investigación presenta el diseño, construcción y validación de un sistema de monitoreo en tiempo real de parámetros ambientales (temperatura, radiación ultravioleta (UV), humedad relativa y del suelo) en huertas de aguacate a cielo abierto. Con una metodología de cuatro fases: 1. Requerimientos del sistema: revisión de la literatura; 2. Establecer los parámetros requeridos: selección de la arquitectura tecnológica, construcción y experimentación del sistema de monitoreo piloto; 3. Diseño de la geometría y construcción: software SolidWorks, contenedor, accesorios y diseño de circuitos en Fritzing; y 4. Validación del prototipo: Implementación y monitoreo del sistema en la huerta de aguacate durante 14 días.",
       },
       {
         id: 17,
         name: "Lisandro Omar Santos Arrieta",
         image: fondo,
-        // semblanza: "Semblanza de Lisandro...",
+        semblanza: "ANÁLISIS DE CONTROL POR MODOS DESLIZANTES EN LA RECUPERACIÓN ESTRUCTURAL CON SISTEMA DE CONTROL PASIVO \n\nEl objetivo de este estudio es analizar los sistemas de Aislamiento Pasivo (SAP) y su respuesta para la recuperación estructural, utilizando el control deslizante (CD) para abordar las distorsiones que se presentan en el balance de energía y alcanzar el punto de equilibrio.",
       },
       {
         id: 18,
         name: "José Antonio Rendón Vargas",
         image: fondo,
+        semblanza: "CLASIFICACIÓN DE LA MADUREZ DEL AGUACATE EN IMÁGENES UTILIZANDO APRENDIZAJE PROFUNDO \n\nEn este pryecto se propone la creacion y entrenamiento de una Red Neuronal Convolucional diseñada para analizar imágenes de alta resolución de aguacates y clasificarlos en diversas etapas de madurez. Este enfoque no solo promete obtener una mejor precisión en la identificación de la madurez del aguacate, sino que también apunta a una gestión más eficiente y sostenible de la cadena de suministro."
       },
       {
         id: 19,
         name: "Imanol Marianito Cuahuitic",
         image: fondo,
+        semblanza: "SISTEMA DE VISIÓN ARTIFICIAL PARA CLASIFICACIÓN DE LA MARUDEZ DEL MANGO \n\nEsta investigación se enfocó en el desarrollo de un sistema de visión artificial para la clasificación del nivel de madurez del mango Ataulfo, evitando el uso de técnicas de Deep Learning en su fase inicial y empleando un perceptrón multicapa (MLP) como modelo base. El sistema abarcó la recolección de imágenes del fruto en cuatro fases de madurez (verde-maduro, parcialmente maduro, maduro firme y maduro suave), su preprocesamiento, segmentación por color y extracción de características para entrenar y evaluar modelos de Machine Learning y de modelos preentrenados MobileNetV2, MobileNetV3 y ResNet18 con el dataset creado."
       },
     ],
   },
@@ -152,21 +154,25 @@ const generaciones = [
         id: 20,
         name: "Yaozihuatl López García",
         image: fondo,
+        semblanza: "TÉCNICAS TEXTILES ARTESANALES PARA EL APROVECHAMIENTO DEL ALGODÓN Y SU IMPACTO AMBIENTAL. \n\nEl impacto ambiental derivado del sector textil a nivel industrial es una situación que merece atención para disminuir esta huella, por lo que esta investigación propone la documentación de la caracterización de técnicas textiles artesanales, el aprovechamiento del algodón y su impacto ambiental que estas técnicas pudieran causar, utilizando el algodón cultivado en la zona amuzga de Xochistlahuaca en la Costa Chica del Estado de Guerrero."
       },
       {
         id: 21,
         name: "Celeste Bello Pastor",
         image: fondo,
+        semblanza: "LA CONSTRUCCIÓN VERNÁCULA RESIDENCIAL EN EL ESTADO DE GUERRERO \n\nEl presente proyecto de investigación propone caracterizar los materiales y técnicas de construcción residencial vernácula en el estado de Guerrero mediante una metodología cualitativa en la recopilación de información y en el cálculo del impacto ambiental, además se realizará la georeferenciación de los materiales y técnicas encontradas, finalmente se documentarán los hallazgos para proyectos futuros que estén relacionados con la construcción tradicional en el estado."
       },
       {
         id: 22,
         name: "Jonathan Jesús Carranza Vega",
         image: fondo,
+        semblanza: "Modelo clasificador de Residuos Solidos Urbanos (RSU) del tipo Madera y Textil mediante Aprendizaje Profundo. \n\nLas capacidades de clasificacion de algunos contenedores inteligentes, mostraron que las  categorías principales de clasificación son: Metal, plástico, vidrio, residuos secos húumedos, reciclable/no reciclable, papel/cartón, biodegradable/no biodegradable, residuos electrónicos, orgánico, mixto madera y separación física (Zoumpoulis et al., 2024). Esta propuesta de investigación desarrolla un modelo clasificador de RSU del tipo madera y textil mediante aprendizaje profundo que permitira abonar a la eficiencia y precisión para una mejor separació adecuada de este tipo de residuos."
       },
       {
         id: 23,
         name: "Orlando Mares Hernández",
         image: fondo,
+        semblanza: "Detección de la madurez del tomate en la planta mediante visión artificial. \n\nEste estudio propone el desarrollo de un sistema basado en visi ́on artificial para la detección de la madurez del tomate en la planta cultivada a campo abierto, utilizando algoritmos de aprendizaje profundo optimizados para operar en entornos agrícolas."
       },
     ],
   },
@@ -177,6 +183,7 @@ const generaciones = [
         id: 24,
         name: "Alejandra Texta Nogueda",
         image: fondo,
+        semblanza: "MODELO DE PLANEACIÓN Y GESTIÓN PARA UNA EMPRESA AGROPECUARIA CASO: EMPRESA MIEL TIERRA GRANDE. \n\nEsta investigación tiene como objetivo, analizar los procesos de planeación y gestión de la empresa apícola Tierra grande ubicada en Hueyitlalpan, municipio de Mártir de Cuilapan, Guerrero para elaborar un modelo de planeación y gestión aplicable a otras empresas. Mediante una metodología de siete etapas: 1. Reconocimiento de la empresa; 2. Documentación de los procesos; 3. Revisión documental de certificaciones para las empresas apícolas; 4. Análisis de los canales de distribución; 5. Revisión documental y/o entrevistas a los directivos para identificar compradores al mayoreo y menudeo; 6. Análisis FODA; y 7. Documentación del modelo de la empresa."
       },
       {
         id: 25,
@@ -192,11 +199,13 @@ const generaciones = [
         id: 27,
         name: "Alberto Barrera Azoños",
         image: fondo,
+        semblanza: "Semáforo Inteligente para la evaluación del riesgo sísmico en zonas urbanas. \n\nLa evaluación rápida de la exposición sísmica de edificios en zona urbanas es crucial para prevenir el riesgo de colapsos, pérdidas humanas y contaminación ambiental. Este proyecto desarrollará un semáforo inteligente de aplicación móvil que evalúe la vulnerabilidad sísmica en edificios en áreas urbanas con una metodología de seis fases: 1. Revisión documental de semáforos inteligentes con aplicación móvil; 2. Análisis cualitativo de metodologías que evalúen la vulnerabilidad; 3. Conformación del Dataset de edificios; 4. Comparación de algoritmos de Redes Neuronales Convulsiónales (CNN); 5. Desarrollo del modelo CNN; y 6. Elaboración y validación de la aplicación móvil para entornos urbanos reales."
       },
       {
         id: 28,
         name: "Joaquín Zueriel Nava Salazar",
         image: fondo,
+        semblanza: "POLÍTICAS PÚBLICAS PARA LA EFICIENCIA ENERGÉTICA DE EDIFICIOS COMERCIALES EN MÉXICO. \n\nEn un mundo donde la EE se ha convertido en algo de suma importancia y donde las reformas han demostrado ser indispensables, este estudio busca ser un referente a futuras normas del sector construcción; por ello se propone analizar las políticas públicas de EE en edificios comerciales implementadas en México. Utilizando una metodología de 5 fases: 1. Revisión del marco normativo mexicano vigente de EE: método PRISMA; 2.  Investigación de políticas públicas de EE: Revisión documental; 3. Verificación de la regulación de políticas públicas gubernamentales: Análisis Estadístico cualitativo y cuantitativo; 4. Análisis de indicadores de EE: Revisión e identificación: 5. Determinación del impacto ambiental y socio-económico: Estimación de indicadores."
       },
     ],
   },
@@ -207,6 +216,7 @@ const generaciones = [
         id: 29,
         name: "Noe Arellano Ávila",
         image: fondo,
+        semblanza: "FUNDAMENTOS PARA LA CONFORMACIÓN DE UNA EMPRESA AGROPECUARIA PRODUCTORA DE AGUACATE. \n\nActualmente la producción de aguacate en la zona serrana tiene dificultades para distribuir su producto, debido a falta de conocimientos de los agricultores en poder consolidar un canal de distribución eficiente. Esta investigación tiene como objetivo establecer los fundamentos de planeación y gestión de una empresa agropecuaria productora de aguacate en el municipio de Leonardo Bravo, Gro., México. Mediante una metodología desarrollada en cuatro etapas: 1. Reconocer la zona productora; 2. Analizar las prácticas de producción; 3. Diseñar el modelo de negocios; y 4. Documentar el modelo de negocios."
       },
     ],
   },
